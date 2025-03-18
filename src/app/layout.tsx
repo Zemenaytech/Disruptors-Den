615eac909a4e1d56e3dc000528d468da619baab4
Abel Tadesse <tadesseabel26@gmail.com>
2025-03-19 01:49:54 +0300

    fix: remove unused dynamic document title update logic in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@
 import type React from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,24 +12,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const path = "/" + pathname.split("/").filter(Boolean)[0];
-
-  useEffect(() => {
-    // Map the pathname to a title
-    const pageTitles: Record<string, string> = {
-      "/": "Home",
-      "/about": "About",
-      "/event": "Event",
-      "/blog": "Blog",
-      "/programs": "Programs",
-      "/login": "Login",
-      "/logout": "Logout",
-    };
-
-    // Update the title dynamically
-    document.title = "TDD | " + pageTitles[path] || "TDD";
-  }, [pathname]);
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} flex flex-col min-h-screen`}>
