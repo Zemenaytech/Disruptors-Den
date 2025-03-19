0099a18ab75e672e461629b65d17f2c116d52dac
Abel Tadesse <tadesseabel26@gmail.com>
2025-03-19 19:10:25 +0300

    test: console log a session to see if it exists

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,8 @@ import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
+  console.log("Middleware triggered for path:", pathname);
+  console.log("Cookies:", request.cookies.getAll());
 
   if (pathname.startsWith("/admin") || pathname.startsWith("/signUp")) {
     const session = await getToken({
